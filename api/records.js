b6f59b2742acdb4374a5bf40a3103ee9776f2eac
SPK80 <psukonkin@gmail.com>
2022-04-05 16:54:54 +0300

    feat: implement records.add fix: change records.get

--- api/records.js
+++ api/records.js
@@ -2,7 +2,11 @@ import { query } from "./mssql.js"
 
 export async function get() {
 	try {
-		const records = await query(`SELECT * FROM records`);
+		const records = await query(`
+SELECT records.id, records.caption, records.filling, records.changed, users.name
+FROM records, users
+WHERE records.author_id=users.id
+		`);
 		if (!records) throw ('Error get records!')
 		return records;
 	} catch (error) {
@@ -10,8 +14,16 @@ export async function get() {
 	}
 }
 
-export function add(record) {
+export async function add(record) {
+	try {
+		const response = await query(`INSERT INTO records (caption, filling, author_id)
+VALUES ('${record.caption}', '${record.filling}', (SELECT id FROM users WHERE name='${record.author}'))`);
 
+		if (!response) throw ('Error get records!')
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 export function del(id) {
@@ -21,3 +33,16 @@ export function del(id) {
 export function update(id, record) {
 
 }
+
+
+//tests add get/////////////////////////////////////
+(async () => {
+	console.log(await add({
+		caption: 'caption3',
+		filling: 'filling3',
+		author: 'quest',
+	}));
+
+	const recs = await get();
+	console.log(recs);
+})()
